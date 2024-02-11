9cb8ebf11b1586193c24050be54d26a9d364a0ca
mWasyluk <marekwasyluk@wp.pl>
2024-02-11 18:08:38 +0100

    ref: refactor to simple init intermediary

--- src/main/java/pl/mwasyluk/ouroom_server/newdomain/member/ChatMemberFactory.java
+++ src/main/java/pl/mwasyluk/ouroom_server/newdomain/member/ChatMemberFactory.java
@@ -1,28 +1,13 @@
 package pl.mwasyluk.ouroom_server.newdomain.member;
 
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.Set;
 
-import org.springframework.stereotype.Component;
 import lombok.NonNull;
 
-import pl.mwasyluk.ouroom_server.newdomain.container.Chat;
 import pl.mwasyluk.ouroom_server.newdomain.user.User;
 
-@Component
-public class ChatMemberFactory implements MembersFactory {
-    @Override
-    public Member createMember(@NonNull User user) {
-        return this.createMember(user, Chat.MEMBER_PRIVILEGES);
-    }
-
-    @Override
-    public Member createMember(@NonNull User user, @NonNull Collection<MemberPrivilege> privileges) {
-        return new ChatMember(user, new HashSet<>(privileges));
-    }
-
-    @Override
-    public Member createAdmin(@NonNull User user) {
-        return new ChatMember(user, Chat.ADMIN_PRIVILEGES);
+public class ChatMemberFactory {
+    public Member create(@NonNull User user, Set<MemberPrivilege> privileges) {
+        return new ChatMember(user, privileges);
     }
 }
