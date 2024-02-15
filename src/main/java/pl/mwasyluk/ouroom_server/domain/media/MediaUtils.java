fc18d66a88e481219c124b26652f667979e0245a
mWasyluk <marekwasyluk@wp.pl>
2024-02-15 21:07:38 +0100

    ref: move factory methods to Media feat: provide sib methods for URL obj

--- src/main/java/pl/mwasyluk/ouroom_server/domain/media/MediaUtils.java
+++ src/main/java/pl/mwasyluk/ouroom_server/domain/media/MediaUtils.java
@@ -9,7 +9,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.multipart.MultipartFile;
 import lombok.NonNull;
 
 public class MediaUtils {
@@ -27,24 +26,6 @@ public class MediaUtils {
         addAll(SUPPORTED_VIDEO_TYPES);
     }};
 
-    public static Media emptyMockOf(MediaType mediaType) {
-        if (isImageType(mediaType)) {
-            Image image = new Image();
-            image.content = "mock".getBytes();
-            image.type = mediaType;
-            return image;
-        }
-
-        if (isVideoType(mediaType)) {
-            Video video = new Video();
-            video.content = "mock".getBytes();
-            video.type = mediaType;
-            return video;
-        }
-
-        throw new IllegalArgumentException("MediaType " + mediaType + " is not supported.");
-    }
-
     public static boolean isMediaTypeSupported(MediaType mediaType) {
         return SUPPORTED_MEDIA_TYPES.contains(mediaType);
     }
@@ -57,64 +38,27 @@ public class MediaUtils {
         return SUPPORTED_VIDEO_TYPES.contains(mediaType);
     }
 
-    public static Media of(byte[] content) throws IOException {
-        if (content == null || content.length == 0) {
-            throw new IllegalArgumentException("Cannot instantiate Media object with empty content.");
-        }
-
-        MediaType mediaType = guessMediaType(content);
-
-        if (isImageType(mediaType)) {
-//            return new Image(type, content);
-            return new Image(content);
-        }
-
-        if (isVideoType(mediaType)) {
-//            return new Video(type, content);
-            return new Video(content);
-        }
-
-        throw new IllegalArgumentException("MediaType " + mediaType + " is not supported.");
-    }
-
-    public static Media of(@NonNull String url) throws IOException {
-        MediaType mediaType = fetchMediaType(url);
-
-        if (isImageType(mediaType)) {
-//            return new Image(type, content);
-            return new Image(url);
-        }
-
-        if (isVideoType(mediaType)) {
-//            return new Video(type, content);
-            return new Video(url);
-        }
-
-        throw new IllegalArgumentException("MediaType " + mediaType + " is not supported.");
-    }
-
-    public static Media from(MultipartFile file) throws IOException {
-        if (file == null || file.isEmpty()) {
-            return null;
-        }
-
-        return of(file.getBytes());
-    }
-
     public static MediaType guessMediaType(byte[] content) throws IOException {
-        return MediaType.parseMediaType(
-                URLConnection.guessContentTypeFromStream(new ByteArrayInputStream(content)));
-    }
-
-    public static MediaType fetchMediaType(String url) throws IOException {
-        BufferedInputStream inputStream = new BufferedInputStream(new URL(url).openStream());
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(content);
         String mimeType = URLConnection.guessContentTypeFromStream(inputStream);
         inputStream.close();
         return MediaType.parseMediaType(mimeType);
     }
 
+    public static MediaType fetchMediaType(String url) throws IOException {
+        return fetchMediaType(new URL(url));
+    }
+
+    public static MediaType fetchMediaType(URL url) throws IOException {
+        return MediaType.parseMediaType(url.openConnection().getContentType());
+    }
+
     public static byte[] fetchContent(String url) throws IOException {
-        BufferedInputStream inputStream = new BufferedInputStream(new URL(url).openStream());
+        return fetchContent(new URL(url));
+    }
+
+    public static byte[] fetchContent(URL url) throws IOException {
+        BufferedInputStream inputStream = new BufferedInputStream(url.openStream());
         byte[] content = inputStream.readAllBytes();
         inputStream.close();
         return content;
