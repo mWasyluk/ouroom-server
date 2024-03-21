2fa97609da923f0028f2577f5ceb526111f52365
mWasyluk <marekwasyluk@wp.pl>
2024-03-21 12:15:04 +0100

    ref: provide static type values

--- src/main/java/pl/mwasyluk/ouroom_server/domain/media/VideoMediaType.java
+++ src/main/java/pl/mwasyluk/ouroom_server/domain/media/VideoMediaType.java
@@ -3,6 +3,9 @@ package pl.mwasyluk.ouroom_server.domain.media;
 import org.springframework.http.MediaType;
 
 public interface VideoMediaType {
-    MediaType MP4 = MediaType.parseMediaType("video/mp4");
-    MediaType MPEG = MediaType.parseMediaType("video/mpeg");
+    String VIDEO_MP4_VALUE = "video/mp4";
+    String VIDEO_MPEG_VALUE = "video/mpeg";
+    
+    MediaType VIDEO_MP4 = MediaType.parseMediaType(VIDEO_MP4_VALUE);
+    MediaType VIDEO_MPEG = MediaType.parseMediaType(VIDEO_MPEG_VALUE);
 }
