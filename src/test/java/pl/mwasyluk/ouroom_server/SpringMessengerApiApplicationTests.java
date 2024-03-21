c4df8a8edceead7f3196296fed7298988f5497dc
mWasyluk <marekwasyluk@wp.pl>
2024-03-21 12:20:19 +0100

    test: apply test profile

--- src/test/java/pl/mwasyluk/ouroom_server/SpringMessengerApiApplicationTests.java
+++ src/test/java/pl/mwasyluk/ouroom_server/SpringMessengerApiApplicationTests.java
@@ -1,9 +1,11 @@
 package pl.mwasyluk.ouroom_server;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class SpringMessengerApiApplicationTests {
 
     @Test
